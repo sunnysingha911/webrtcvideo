23f4b16dba506e07d4f3b2236a589bf1f0a42d51
Sunnysingha <sunnysingha911@gmail.com>
2020-06-15 18:40:22 +0530

    Updates

---
+++ precache-manifest.d0c1b14e59b9557d65e8d1fd08051822.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f523421f5b0eed35cdd8d90d29a354c",
+    "url": "/webrtcvideo/index.html"
+  },
+  {
+    "revision": "df7e4835eac79569b38d",
+    "url": "/webrtcvideo/static/css/main.ead53d46.chunk.css"
+  },
+  {
+    "revision": "79db9f89050d7f40eb07",
+    "url": "/webrtcvideo/static/js/2.fbd0147f.chunk.js"
+  },
+  {
+    "revision": "451dbc5027c3dfdfc0243ef93b8a192d",
+    "url": "/webrtcvideo/static/js/2.fbd0147f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df7e4835eac79569b38d",
+    "url": "/webrtcvideo/static/js/main.d189fe5c.chunk.js"
+  },
+  {
+    "revision": "009dd2dcdbef5f7aa753",
+    "url": "/webrtcvideo/static/js/runtime-main.a3eddbcf.js"
+  }
+]);
